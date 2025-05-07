0bc8058147db27433c96e5dc1fef02f2a6fa5264
MaxAdmin <maxeduit2@gmail.com>
2025-05-07 15:08:40 +0300

    07_wall.py is DONE

--- lesson_003/07_wall.py
+++ lesson_003/07_wall.py
@@ -1,11 +1,28 @@
 # -*- coding: utf-8 -*-
 
 # (цикл for)
-import simple_draw
+import simple_draw as sd
+from simple_draw import COLOR_PURPLE
 
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
 
-# TODO здесь ваш код
+for y in range(0, 700, 50):
+    for x in range(-50, 700, 100):
+        start_point = sd.get_point(x, y)
+        end_point = sd.get_point(x + 100, y)
+        sd.line(start_point=start_point, end_point=end_point, color=COLOR_PURPLE, width=2)
 
-simple_draw.pause()
+for x in range(-50, 700, 100):
+    for y in range(0, 700, 100):
+        start_point = sd.get_point(x, y)
+        end_point = sd.get_point(x, y + 50)
+        sd.line(start_point=start_point, end_point=end_point, color=COLOR_PURPLE, width=2)
+
+for x in range(0, 700, 100):
+    for y in range(50, 700, 100):
+        start_point = sd.get_point(x, y)
+        end_point = sd.get_point(x, y + 50)
+        sd.line(start_point=start_point, end_point=end_point, color=COLOR_PURPLE, width=2)
+
+sd.pause()
